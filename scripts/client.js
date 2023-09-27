9ae71077292bf246f7ef0c1ccb711545bc17d4ed
Augusto Vieira <88207253+GutoVieoli@users.noreply.github.com>
2023-09-27 13:49:54 -0300

    Versao basica 2
    Ja é possivel digitar o ip ao qual voce quer conectar, e quando há 2 clientes conectados, o serivdor envia uma mensagem para todos

--- scripts/client.js
+++ scripts/client.js
@@ -1,27 +1,55 @@
-const HOST = 'ws://localhost:3000'; // Endereço do servidor WebSocket
-const socket = new WebSocket(HOST);
+let HOST; // = 'ws://x:3000'; // Endereço do servidor WebSocket
+let socket; // = new WebSocket(HOST);
+var conexao = false;
 
-socket.onopen = (event) => {
-  console.log('Conectado ao servidor.');
-  // Envie uma mensagem para o servidor quando a conexão estiver aberta
-  socket.send('Olá, servidor!');
-};
+function obterEnderecoIP() {
+  event.preventDefault()
+  var enderecoIP = document.getElementById('ip').value;
+  //alert('Endereço IP digitado: ' + enderecoIP);
+  HOST = 'ws://' + enderecoIP + ':3000';
+  socket = new WebSocket(HOST);
+  conexao = true;
+  console.log(HOST);
+  fio1();
 
-socket.onmessage = (event) => {
-  const message = event.data;
-  console.log(`Mensagem do servidor: ${message}`);
-  // Encerre a conexão após receber uma resposta (opcional)
-  //socket.close();  ------------
-};
+}
 
-socket.onclose = (event) => {
-  if (event.wasClean) {
-    console.log('Conexão fechada de forma limpa.');
-  } else {
-    console.error('Conexão interrompida abruptamente.');
-  }
-};
+function fio1(){
 
-socket.onerror = (error) => {
-  console.error(`Erro: ${error.message}`);
-};
+    socket.onopen = (event) => {
+      console.log('Conectado ao servidor.');
+      // Envie uma mensagem para o servidor quando a conexão estiver aberta
+      socket.send('Olá, servidor!');
+    };
+
+    //setInterval(mnsg, 10000);
+    
+    
+    socket.onmessage = (event) => {
+      const message = event.data;
+      console.log(`Mensagem do servidor: ${message}`);
+      // Encerre a conexão após receber uma resposta (opcional)
+      //socket.close();  ------------
+    
+      // Seleciona o elemento <nav> pelo seu ID
+      var navElement = document.getElementById("message");
+      navElement.innerHTML = "Mensagem do server: " + message;
+    };
+    
+    
+    socket.onclose = (event) => {
+      if (event.wasClean) {
+        console.log('Conexão fechada de forma limpa.');
+      } else {
+        console.error('Conexão interrompida abruptamente.');
+      }
+    };
+    
+    
+    socket.onerror = (error) => {
+      console.error(`Erro: ${error.message}`);
+    };
+
+}
+
+
--- scripts/server.js
+++ scripts/server.js
@@ -2,21 +2,48 @@ const WebSocket = require('ws');
 
 const server = new WebSocket.Server({ port: 3000 });
 
+// Array para armazenar as conexões dos clientes
+const clientes = [];
+
 server.on('connection', (ws) => {
+
+  // Adcione a conexão do cliente ao array
+  clientes.push(ws);
   console.log('Cliente conectado.');
+  //console.log(clientes)
+
 
   // Evento de recebimento de mensagens do cliente
   ws.on('message', (message) => {
     console.log(`Mensagem do cliente: ${message}`);
 
     // Envie uma mensagem de volta para o cliente
-    ws.send('Dobra e dá pro próximo!');
+    ws.send('Aguardando o numero correto de jogadores para iniciar!');
   });
 
+
   // Evento de fechamento da conexão
-//   ws.on('close', () => {
-//     console.log('Cliente desconectado.');
-//   });
+  ws.on('close', () => {
+    console.log('Cliente desconectado.');
+
+    // Remova o cliente do array
+    clientes.splice(clientes.indexOf(ws), 1);
+  });
+
+  // Verifique se há 2 clientes conectados e envie uma mensagem para ambos
+  if (clientes.length === 2) {
+    setTimeout(enviarMensagemParaTodos, 1000);
+  }
+  console.log(clientes.length)
+
+
 });
 
 console.log('Servidor WebSocket ouvindo na porta 3000');
+
+// Função para enviar uma mensagem para todos os clientes conectados
+function enviarMensagemParaTodos(mensagem) {
+  clientes.forEach((clients) => {
+    clients.send('Ja temos 2 Players, vamos começar!!');
+  });
+}
